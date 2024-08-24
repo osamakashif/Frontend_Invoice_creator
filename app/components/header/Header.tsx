4fa47fd34b67abacced4fa7dbfab426e9ac7df75
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2024-08-25 08:30:34 +1200

    [style]: Colour scheme.
    Colour scheme.

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -2,7 +2,7 @@ import { Navbar } from "../navbar/Navbar";
 
 export const Header = () => {
   return (
-    <div className="fixed top-0 w-full p-2">
+    <div className="fixed top-0 w-full p-2 secondary-colour">
       <Navbar />
     </div>
   );
