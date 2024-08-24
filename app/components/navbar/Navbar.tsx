f201bdcb5782c01296a4c70fd65f2573e1b78ffa
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2024-08-25 01:11:51 +1200

    [feat]: Update Navbar.
    Update Navbar.

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -4,17 +4,32 @@ import "./Navbar.css";
 
 export const Navbar = () => {
   const router = useRouter();
+  const routes = [
+    { name: "Home", path: "/" },
+    { name: "Individual", path: "/individual" },
+    { name: "Group", path: "/group" },
+  ];
+  const title: string = "Invoice Creator";
   return (
     <div className="navbar">
-      <a type="button" onClick={() => router.push("/")}>
-        Home
-      </a>
-      <a type="button" onClick={() => router.push("/individual")}>
-        Individual
-      </a>
-      <a type="button" onClick={() => router.push("/group")}>
-        Group
-      </a>
+      <h1>
+        <a className="nav-link" onClick={() => router.push("/")}>
+          {title}
+        </a>
+      </h1>
+      <div>
+        {routes.map((route) => {
+          return (
+            <a
+              className="nav-link px-2"
+              onClick={() => router.push(route.path)}
+              key={route.name}
+            >
+              {route.name}
+            </a>
+          );
+        })}
+      </div>
     </div>
   );
 };
