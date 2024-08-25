aa5749e310b4ac013d1b37273a945fe85fbad18b
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2024-08-25 20:58:13 +1200

    [feat]: Calculate income tax.
    Calculate income tax.

--- domain/invoice.ts
+++ domain/invoice.ts
@@ -1,3 +1,8 @@
+interface TaxBracket {
+  limit: number | undefined;
+  rate: number;
+}
+
 export class Invoice {
   name: string;
   email: string;
@@ -17,7 +22,31 @@ export class Invoice {
   }
 
   private calculateIncomeTax = (revenue: number): number => {
-    // TO DO
-    return 0;
+    let incomeTax: number = 0;
+    let valueAlreadyTaxed: number = 0;
+    const taxBrackets: TaxBracket[] = [
+      { limit: 1950000, rate: 0.05 },
+      { limit: 3300000, rate: 0.1 },
+      { limit: 6950000, rate: 0.2 },
+      { limit: 9000000, rate: 0.23 },
+      { limit: 18000000, rate: 0.33 },
+      { limit: 40000000, rate: 0.4 },
+      { limit: undefined, rate: 0.45 },
+    ];
+    taxBrackets.forEach((taxBracket: TaxBracket) => {
+      const taxBracketRate: number = taxBracket.rate;
+      if (taxBracket.limit) {
+        const taxBracketLimit: number = taxBracket.limit;
+        if (revenue < taxBracketLimit) {
+          incomeTax += (revenue - valueAlreadyTaxed) * taxBracketRate;
+          return incomeTax;
+        }
+        incomeTax += (taxBracketLimit - valueAlreadyTaxed) * taxBracketRate;
+        valueAlreadyTaxed += taxBracketLimit;
+      } else {
+        incomeTax += (revenue - valueAlreadyTaxed) * taxBracketRate;
+      }
+    });
+    return incomeTax;
   };
 }
