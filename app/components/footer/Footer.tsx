c4790fc227925ed5dcbc0d634171e412aefb4cd0
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2024-08-26 21:19:05 +1200

    [style]: Footer position.
    Adjust Footer position.

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
-    <div className="w-full text-center p-2 secondary-colour">
-      <p>Invoice Creator © 2024</p>
+    <div className="w-full text-center p-2 secondary-colour fixed bottom-0">
+      <p>© 2024 Invoice Creator</p>
     </div>
   );
 };
