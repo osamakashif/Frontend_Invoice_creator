a28ebcfbe497766ecd20a270f106b1a711ec09ae
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2024-08-27 01:42:44 +1200

    [dist]: Config for GitHub pages deployment.
    Config for GitHub pages deployment.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/frontend-invoice-creator",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
